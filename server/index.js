374db7f40f7706380a3260d0554998e6287db729
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-07-07 16:25:14 -0400

    Adding cors into server

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const Event = require('./models/eventSchema');
 const Conversation = require('./models/conversationSchema');
 const Message = require('./models/messageSchema');
 const bodyParser = require('body-parser')
+const cors = require('cors');
 
 mongoose.connect(keys.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -18,20 +19,13 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
 }))
 
-
 app.use(mainRoutes);
+app.use(cors);
 
 //for formatting responses?
 const formatMessage = (username, text) => {
