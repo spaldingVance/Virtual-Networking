d28a81ebcfc7a9d34e455bff69f6c7a044032127
Pierre Lourens <plourens3@gmail.com>
2020-07-07 08:36:43 -0400

    sets up components & redux

---
+++ server/client/src/components/ChatBoxesContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChatBoxesContainer = () => {
+  // TODO
+};
+
+export default ChatBoxesContainer;
---
+++ server/client/src/components/EventButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EventButton = () => {
+  // TODO
+};
+
+export default EventButton;
