8c1e02fe342bc4ad3c4bcec8eb271ee420bf30ea
Pierre Lourens <plourens3@gmail.com>
2020-07-07 16:59:11 -0400

    stashing changes before merging master

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const ROOT_URL = `https://localhost:5000/`;
+const ROOT_URL = `http://localhost:5000`;
 
 export const GET_EVENTS = "GET_EVENTS";
 export const SELECT_EVENT = "SELECT_EVENT";
@@ -7,6 +7,8 @@ export const SELECT_EVENT = "SELECT_EVENT";
 export function getEvents() {
   const url = `${ROOT_URL}/events`;
   const request = axios.get(url);
+
+  request.then(console.log("hi"));
 
   return {
     type: GET_EVENTS,
--- server/client/src/reducers/eventsReducer.js
+++ server/client/src/reducers/eventsReducer.js
@@ -3,6 +3,7 @@ import { GET_EVENTS } from "../actions/index";
 export default function (state = [], action) {
   switch (action.type) {
     case GET_EVENTS:
+      console.log(action.payload);
       return [action.payload.data];
     default:
       return state;
