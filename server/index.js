e90680763f1e0913b03849284aceb52638b9562f
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-07-07 10:05:14 -0400

    added database schemas, changed file names in models folder

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const socketio = require('socket.io');
 const moment = require ('moment');
-const User = require('../models/user');
-const Event = require('../models/event');
-const Conversation = require('../models/conversation');
-const Message = require('../models/message');
+const User = require('../models/userSchema');
+const Event = require('../models/eventSchema');
+const Conversation = require('../models/conversationSchema');
+const Message = require('../models/messageSchema');
 
 mongoose.connect(keys.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
