43e9ea347eb4d50c2df43ec872365cc95e7fb6ac
Pierre Lourens <plourens3@gmail.com>
2020-07-08 18:14:46 -0400

    Fixes conversation list loading

--- server/client/src/components/CurrentEventHeader.js
+++ server/client/src/components/CurrentEventHeader.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
 
 // styling imports
 import "../styles/CurrentEventHeader.css";
 
-const CurrentEventHeader = () => {
-  // TODO
+const CurrentEventHeader = (props) => {
+  console.log("The current event is ", props.currentEvent);
   return (
     <div>
       <div id="current-event-tile">
-        <h3>
-          You are attending <span>Event Name Here</span>
-        </h3>
+        <h2>You are attending</h2> <h1>Event Name Here</h1>
       </div>
     </div>
   );
 };
 
-export default CurrentEventHeader;
+function mapStateToProps(state) {
+  return { currentEvent: state.currentEvent };
+}
+
+export default connect(mapStateToProps)(CurrentEventHeader);
