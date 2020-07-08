bd50280026e6034d3164b6cefcc01488db34264f
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-07-08 09:55:43 -0400

    removing duplicate cors

--- server/index.js
+++ server/index.js
@@ -20,8 +20,6 @@ mongoose.connect(keys.MONGODB_URI, {
 
 app.use(cors());
 
-app.use(cors());
-
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = socketio(server);
