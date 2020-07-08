bdd977bb76c96d97d08213c46f836e53e23d144e
Joe Krall <kralljr@outlook.com>
2020-07-08 08:58:16 -0400

    added additional css but commented most of it out. Position: absolute and bottom:0 again.

--- server/client/src/components/EventsList.js
+++ server/client/src/components/EventsList.js
@@ -21,26 +21,54 @@ class EventsList extends Component {
       <Col lg={3} md={4} sm={6} xs={12} className="event-tile-column mb-4">
         <Container className="event-tile">
           <Row >
-            <Col className="pt-3">
+            <Col className="event-tile-top">
               {/* events.eventName */}
               {/* 900 Roboto for All Sans Serif */}
               <h3>THIS IS A LONG, VERY LONG EVENT NAME</h3>
             </Col>
           </Row>
+          <div className="event-tile-bottom">
+
+            <Row >
+              <Col className="d-inline-flex justify-content-start align-items-baseline ">
+                {/* For each participant, reduce to a number */}
+                <FontAwesomeIcon icon={faUsers} />{" "}
+                <p className="event-tile-numbers">23</p>
+              </Col>
+              <Col className="d-inline-flex justify-content-end align-items-baseline">
+                {/* For each conversation, reduce to a number */}
+                <FontAwesomeIcon icon={faComments} />{" "}
+                <p className="event-tile-numbers ">24</p>
+              </Col>
+            </Row>
+          </div>
+        {/* <Container className="event-tile"> */}
+          {/* <Row >
+            <Col className="pt-3"> */}
+              {/* events.eventName */}
+              {/* 900 Roboto for All Sans Serif */}
+              {/* <div className="event-tile-top">
+              <h3>THIS IS A LONG, VERY LONG EVENT NAME</h3>
+              </div> */}
+            {/* </Col>
+          </Row> */}
           
 
-          <Row className="event-tile-bottom">
-            <Col className="d-inline-flex justify-content-start align-items-baseline ">
-              {/* For each participant, reduce to a number */}
-              <FontAwesomeIcon icon={faUsers} />{" "}
+
+          {/* <div className="event-tile-bottom">
+            {/* For each participant, reduce to a number */}
+            {/* <div className="event-tile-bottom-people">
+              <FontAwesomeIcon icon={faUsers} />
               <p className="event-tile-numbers">23</p>
-            </Col>
-            <Col className="d-inline-flex justify-content-end align-items-baseline">
-              {/* For each conversation, reduce to a number */}
+            </div> */}
+            {/* For each conversation, reduce to a number */}
+            {/* <div className="event-tile-bottom-conversations">
               <FontAwesomeIcon icon={faComments} />{" "}
               <p className="event-tile-numbers ">24</p>
-            </Col>
-          </Row>
+            </div>
+          </div>
+        </Container> */} 
+
         </Container>
       </Col>
     );
