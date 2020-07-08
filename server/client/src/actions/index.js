751f3e61e89e3080b0ba5cf7810d2009d83023cd
MCola <mcolapietro502@gmail.com>
2020-07-08 13:52:22 -0400

    adjusted rootURL again

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-//const ROOT_URL = `http://localhost:5000`; commenting out for deployment
+const ROOT_URL = `http://localhost:5000`;
 
-const ROOT_URL = ``;
+//const ROOT_URL = ``
 
 
 export const GET_EVENTS = "GET_EVENTS";
@@ -9,14 +9,14 @@ export const SELECT_EVENT = "SELECT_EVENT";
 export const GET_CONVERSATIONS = "GET_CONVERSATIONS";
 
 export function getEvents() {
-  const url = `/events`;
+  const url = `${ROOT_URL}/events`;
   const request = axios.get(url);
 
   request.then(console.log("hi!!!"));
 
   return {
     type: GET_EVENTS,
-    payload: request,
+    payload: request
   };
 }
 
@@ -25,20 +25,19 @@ export function selectEvent() {
 
   return {
     type: SELECT_EVENT,
-    payload: request,
+    payload: request
   };
 }
 
 export function getConversations() {
   // hard coding convo ID for testing
-  // we need to get the conversations by
-  const url = `/events/5f04d44afdc420235c69ca26`;
+  const url = `${ROOT_URL}/events/5f0517a09e543554fcb133e4`;
   const request = axios.get(url);
 
-  request.then(console.log("the request is being made for conversations"));
+  request.then(console.log("conversations retrieved"));
 
   return {
     type: GET_CONVERSATIONS,
-    payload: request,
+    payload: request
   };
 }
