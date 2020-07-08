0380e7b8d52289365a66b2f6532cc6397ae2e81d
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-07-07 22:52:13 -0400

    convo action and reducer set up but network error

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import eventsReducer from "./eventsReducer";
+import conversationsReducer from "./conversationsReducer";
 
 const rootReducer = combineReducers({
   events: eventsReducer,
+  conversations: conversationsReducer
 });
 
 export default rootReducer;
