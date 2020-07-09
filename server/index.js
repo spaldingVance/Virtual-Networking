ee5ae6142a8eafaed9c247978f20eb43aa58a0bc
Pierre Lourens <plourens3@gmail.com>
2020-07-08 21:34:22 -0400

    Changes * to / for app.get

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   // Express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require("path");
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
