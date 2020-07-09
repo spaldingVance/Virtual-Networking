eee1fe26087e6923b119c14930a332bd9fd1edda
Pierre Lourens <plourens3@gmail.com>
2020-07-08 22:02:51 -0400

    moved mainRoutes to try that

--- server/index.js
+++ server/index.js
@@ -19,6 +19,7 @@ mongoose.connect(keys.MONGODB_URI, {
 });
 
 app.use(cors());
+mainRoutes(app);
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
@@ -28,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
   // Express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require("path");
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
@@ -43,8 +44,6 @@ app.use(
     extended: true,
   })
 );
-
-mainRoutes(app);
 
 //for formatting responses?
 const formatMessage = (username, text) => {
